6bf6e3717e9be8d606687364abaf1d111275b21e
jipark <hmnc21@gmail.com>
2018-03-05 18:09:28 +0900

    Initial commit

---
+++ app/src/main/java/gympilot/sundayfactory/com/gympilot/viewers/gymlist/fragment_gymlist.java
@@ -0,0 +1,10 @@
+package gympilot.sundayfactory.com.gympilot.viewers.gymlist;
+
+import gympilot.sundayfactory.com.gympilot.viewers.BaseFragment;
+
+/**
+ * Created by jipark on 2018-02-22.
+ */
+
+public class fragment_gymlist extends BaseFragment {
+}
---
+++ app/src/main/java/gympilot/sundayfactory/com/gympilot/viewers/gymplaylist/fragment_gymplaylist.java
@@ -0,0 +1,10 @@
+package gympilot.sundayfactory.com.gympilot.viewers.gymplaylist;
+
+import gympilot.sundayfactory.com.gympilot.viewers.BaseFragment;
+
+/**
+ * Created by jipark on 2018-02-22.
+ */
+
+public class fragment_gymplaylist extends BaseFragment {
+}
